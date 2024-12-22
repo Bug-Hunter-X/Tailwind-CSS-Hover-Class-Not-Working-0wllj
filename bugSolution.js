795dabd27d2504a522f8abfb82f54ad6fa2026fb
BugHunter-X <askzenai@gmail.com>
2024-12-23 00:16:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,7 @@
+```javascript
+//The issue was solved by correctly configuring the CSS order.
+<div class="p-4 bg-red-500 transition duration-300 ease-in-out hover:bg-blue-700">
+  Hover over me!
+</div>
+//Adding transition, duration and ease-in-out classes will fix the issue. 
+```
